a37324750fa714de7b589d814f67b54da5823d9f
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-10-25 08:34:31 -0300

    Adicionando exemplo de pilha

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<Pessoa> minhaLista = new ArrayList<>();
+        Stack<String> pilha = new Stack<>();
+        pilha.push("João");
+        pilha.push("Maria");
+        pilha.push("Pedro");
 
-        minhaLista.add(new Pessoa("111.111.111-01", "João"));
-        minhaLista.add(new Pessoa("222.222.222-02", "Maria"));
+        System.out.println("Topo da pilha: "+pilha.peek());
 
-        minhaLista.add(0, new Pessoa("333.333.333-03", "Pedro"));
-
-        System.out.println(minhaLista);
-
-        System.out.println("Tamanho: "+minhaLista.size());
-
-        System.out.println(minhaLista.contains(
-                new Pessoa("111.111.111-01", "João")));
-
-        System.out.println(minhaLista.indexOf(new Pessoa(
-                "333.333.333-03", "Pedro")));
-
-        //Só funciona de 0 até size()
-        minhaLista.add(3, new Pessoa("444.444.444-04",
-                "José"));
-
-        System.out.println(minhaLista);
+        while(!pilha.empty()){
+            System.out.println(pilha.pop());
+        }
 
 
     }
