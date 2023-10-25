28aacf42a41df464be3c6dd01afe2860dad42e66
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-10-25 08:17:55 -0300

    Atualizando exemplo para usar pessoas

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,26 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<String> minhaLista = new ArrayList<>();
+        List<Pessoa> minhaLista = new ArrayList<>();
 
-        minhaLista.add("João");
-        minhaLista.add("Maria");
-        minhaLista.add(0, "Pedro");
+        minhaLista.add(new Pessoa("111.111.111-01", "João"));
+        minhaLista.add(new Pessoa("222.222.222-02", "Maria"));
+
+        minhaLista.add(0, new Pessoa("333.333.333-03", "Pedro"));
 
         System.out.println(minhaLista);
 
-        System.out.println(minhaLista.get(2));
-        System.out.println(minhaLista.set(1, "João da Silva"));
-        minhaLista.remove("Pedro");
+        System.out.println("Tamanho: "+minhaLista.size());
+
+        System.out.println(minhaLista.contains(
+                new Pessoa("111.111.111-01", "João")));
+
+        System.out.println(minhaLista.indexOf(new Pessoa(
+                "333.333.333-03", "Pedro")));
+
+        //Só funciona de 0 até size()
+        minhaLista.add(3, new Pessoa("444.444.444-04",
+                "José"));
 
         System.out.println(minhaLista);
 
---
+++ src/Pessoa.java
@@ -0,0 +1,50 @@
+import java.util.Objects;
+
+public class Pessoa {
+
+    private String cpf;
+    private String nome;
+
+    public Pessoa(String cpf, String nome) {
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return Objects.equals(cpf, pessoa.cpf) &&
+                Objects.equals(nome, pessoa.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpf, nome);
+    }
+}
